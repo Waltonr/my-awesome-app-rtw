c8e696af3ce9494355664513c326899697c87314
Waltonr <waltonr18126@gmail.com>
2022-05-23 12:50:44 -0400

    refactor: changed hello message

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express()
 
 // Simple Hello World Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Raven! Nice to see you!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port: ${APP_PORT}`)
 })
